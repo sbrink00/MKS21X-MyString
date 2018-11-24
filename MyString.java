fbee2d93689d82fdf09d0f25c9475201045f5c66
sbrink00 <sbrink00@stuy.edu>
2018-11-24 13:41:40 -0500

    fixed compareTo. completed MyString class

--- MyString.java
+++ MyString.java
@@ -3,20 +3,31 @@ public class MyString implements CharSequence, Comparable<CharSequence>{
 
   public static void main(String[]args){
     MyString a = new MyString("hello");
-    //System.out.println(a.subSequence(1,3));
-    //a = a.subSequence(1,3);
-    //System.out.println(a);
-    //System.out.println(a);
-    //System.out.println(a.toString());
-    //System.out.println(a.length());
-    //System.out.println(a.charAt(3));
-    //System.out.println(a.subSequence(0,3));
-    //a = new MyString(a.subSequence(0,3));
-    //System.out.println(testNum);
-    MyString s1 = new MyString("bbbbbb");
-    MyString s2 = new MyString("a");
-    System.out.println(s1.compareTo(s2));
-    System.out.println("bbbbbb".compareTo("a"));
+    System.out.println(a);
+    System.out.println(a.toString());
+    System.out.println(a.length());
+    System.out.println(a.charAt(4));
+    System.out.println(a.subSequence(1,4));
+    a = a.subSequence(1,4);
+    System.out.println(a);
+    System.out.println("testing compare to\n");
+    //MyString ms1 = new MyString("hellabc");
+    //MyString ms2 = new MyString("hell");
+    //System.out.println(ms1.compareTo(ms2));
+    String[][] comparables = {{"a", "c"}, {"hello", "hello"}, {"123", "123"}, {"12", "32"}, {"32", "12"}, {"hellabc", "hell"}, {"hell", "hellabc"}, {"90[]98*&^", "*(*^5{}|)"}};
+    //for loop to test compare to with different values.
+    //It tests myString compare to with string compare to to make sure they're the same
+    for (int row = 0; row < comparables.length; row ++){
+      String s1 = comparables[row][0];
+      String s2 = comparables[row][1];
+      MyString ms1 = new MyString(comparables[row][0]);
+      MyString ms2 = new MyString(comparables[row][1]);
+      System.out.println(s1.compareTo(s2) == ms1.compareTo(ms2));
+    }
+
+
+    System.out.println("a".compareTo("p"));
+    System.out.println();
   }
 
   public MyString(CharSequence s){
@@ -54,7 +65,7 @@ public class MyString implements CharSequence, Comparable<CharSequence>{
     int limit;
     if (this.length() > other.length()) limit = other.length();
     else limit = this.length();
-    for (int idx = 0; idx < this.length(); idx ++){
+    for (int idx = 0; idx < limit; idx ++){
       char thisCurrentChar = this.charAt(idx);
       char otherCurrentChar = other.charAt(idx);
       int me = (int) thisCurrentChar;
